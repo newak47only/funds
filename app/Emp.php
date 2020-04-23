54d814637dce289f530390c3c371df18d74c4064
vagrant <vagrant@homestead>
2020-04-23 09:43:41 +0000

    用户认证

---
+++ app/Emp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Emp extends Authenticatable
+{
+    //
+    use Notifiable;
+
+    protected $table="emp";
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+     protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
